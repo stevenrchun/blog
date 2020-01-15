cdf4829bd8838b7f16e9b4fea9506dbbfcca22ef
Steven Chun <steven.r.chun.19@dartmouth.edu>
2020-01-14 23:31:55 -0800

    add local ghost integration

---
+++ scripts/ghost.js
@@ -0,0 +1,38 @@
+const ghostContentAPI = require("@tryghost/content-api");
+
+const api = new ghostContentAPI({
+  url: "http://localhost:2368",
+  key: "9e460c796199a851d3d272388b",
+  version: "v3"
+});
+
+const ghostPostData = async () => {
+  const data = await api.posts
+    .browse({
+      limit: "all",
+      include: "tags,authors"
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
+  data.forEach(post => {
+    tags = [];
+    post.tags.forEach(function(tag) {
+      // Space is neccesary or else Hexo concatenates the tag names.
+      tags.push(tag.name + " ");
+    });
+    const postData = {
+      title: post.title,
+      slug: post.slug,
+      path: post.slug,
+      tags: tags,
+      date: post.published_at,
+      content: post.html
+    }
+
+    hexo.post.create(postData, true);
+  });
+};
+
+ghostPostData();
---
+++ source/_posts/test-ghost-post.md
@@ -0,0 +1,9 @@
+---
+title: Test Ghost Post
+tags:
+  - 'Memez '
+  - 'draft '
+date: 2020-01-14 21:50:05
+---
+
+<p>Local install of ghost to hexo script, let's see if it works!</p><hr><p>Very exciting stuff</p>
