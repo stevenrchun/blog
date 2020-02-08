885e15abfe37ddff82e775d230a8b7491391902a
Steven Chun <steven.r.chun.19@dartmouth.edu>
2020-02-08 00:28:36 -0800

    add tags, add blurb

--- scripts/ghost.js
+++ scripts/ghost.js
@@ -1,26 +1,26 @@
-const ghostContentAPI = require("@tryghost/content-api");
+const ghostContentAPI = require('@tryghost/content-api');
 
 const api = new ghostContentAPI({
-  url: "http://localhost:2368",
-  key: "9e460c796199a851d3d272388b",
-  version: "v3"
+  url: 'http://localhost:2368',
+  key: '9e460c796199a851d3d272388b',
+  version: 'v3',
 });
 
 const ghostPostData = async () => {
-  const data = await api.posts
-    .browse({
-      limit: "all",
-      include: "tags,authors"
-    })
-    .catch(err => {
-      console.error(err);
-    });
+  const data = await api.posts.browse({
+    limit: 'all',
+    include: 'tags,authors',
+  });
+  // We want to exit if Ghost isn't up.
+  //.catch((err) => {
+  //   console.error(err);
+  // });
 
-  data.forEach(post => {
+  data.forEach((post) => {
     tags = [];
     post.tags.forEach(function(tag) {
       // Space is neccesary or else Hexo concatenates the tag names.
-      tags.push(tag.name + " ");
+      tags.push(tag.name + ' ');
     });
     const postData = {
       title: post.title,
@@ -28,9 +28,8 @@ const ghostPostData = async () => {
       path: post.slug,
       tags: tags,
       date: post.published_at,
-      content: post.html
-    }
-
+      content: post.html,
+    };
     hexo.post.create(postData, true);
   });
 };
